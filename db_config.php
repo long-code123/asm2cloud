5b20689216ec98f9bce579a50d94f155e2e0dc4f
Long Nguyễn <long@gmail.com>
2022-07-15 00:08:39 +0700

    first commit

---
+++ db_config.php
@@ -0,0 +1,8 @@
+<?php
+$host_name = "ec2-3-219-229-143.compute-1.amazonaws.com";
+$db_name = "d4jolltube5v8d";
+$db_user = "cblrnpyylntygq";
+$db_passwd = "7a0866db237cac1437fa6396a508697ab863cf2deab6186bb180f2ed935c6e2a";
+$db_conn_string = "host=$host_name dbname=$db_name user=$db_user password=$db_passwd";
+?>
+
